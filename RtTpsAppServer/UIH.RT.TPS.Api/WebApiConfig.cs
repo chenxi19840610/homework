59419bba7e32b25a90e7e74c28da49c4af0b813f
chenxi19840610 <chenxi19840610@sina.com>
2015-07-11 12:07:53 +0800

    add web api project

---
+++ RtTpsAppServer/UIH.RT.TPS.Api/WebApiConfig.cs
@@ -0,0 +1,44 @@
+﻿using Autofac;
+using Autofac.Integration.WebApi;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace UIH.RT.TPS.Api
+{
+    public class WebApiConfig
+    {
+        public static void Initialize(IAppBuilder app)
+        {
+            var builder = new ContainerBuilder();
+
+            // STANDARD WEB API SETUP:
+
+            // Get your HttpConfiguration. In OWIN, you'll create one
+            // rather than using GlobalConfiguration.
+            var config = new HttpConfiguration();
+
+            // Register your Web API controllers.
+            builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
+
+            // Run other optional steps, like registering filters,
+            // per-controller-type services, etc., then set the dependency resolver
+            // to be Autofac.
+            var container = builder.Build();
+            config.DependencyResolver = new AutofacWebApiDependencyResolver(container);
+
+            // OWIN WEB API SETUP:
+
+            // Register the Autofac middleware FIRST, then the Autofac Web API middleware,
+            // and finally the standard Web API middleware.
+            app.UseAutofacMiddleware(container);
+            app.UseAutofacWebApi(config);
+            app.UseWebApi(config);
+        }
+    }
+}
---
+++ RtTpsAppServer/UIH.RT.TPS.WebHost/Startup.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using UIH.RT.TPS.Api;
+
+[assembly: OwinStartup(typeof(UIH.RT.TPS.WebHost.Startup))]
+
+namespace UIH.RT.TPS.WebHost
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // 有关如何配置应用程序的详细信息，请访问 http://go.microsoft.com/fwlink/?LinkID=316888
+            WebApiConfig.Initialize(app);
+        }
+    }
+}
